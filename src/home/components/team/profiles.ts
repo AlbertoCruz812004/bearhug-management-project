de035b64c04aee7459ff81d889aef3042a1030b4
alexprojectspace <alexis.gm.contact@gmail.com>
2025-02-23 14:15:34 -0600

    style: actualizar fotos de perfil, descripción y titulo de la página

--- src/home/components/team/profiles.ts
+++ src/home/components/team/profiles.ts
@@ -1,32 +1,32 @@
 const ALEX_PROFILE = {
-    photo: "https://images3.alphacoders.com/132/1322715.png",
+    photo: "/src/home/components/team/img/alexis.png",
     name: "Alexis Garcia",
     rol: "Project Manager",
-    biography: "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    biography: "Planifico y coordino el proyecto, asegurándome de que todo se haga según lo previsto y dentro de los plazos. Además, colaboro en el diseño de la web, ayudando a dar forma a su apariencia y experiencia",
     socialMediaUrl: ["#","#","#"]
 }
 
 const DARIANA_PROFILE = {
-    photo: "https://i.pinimg.com/564x/29/60/7b/29607bf1879a361905dfa20f7e1a4be8.jpg",
+    photo: "/src/home/components/team/img/dariana.png",
     name: "Dariana Nahomi",
-    rol: "Web Designer",
-    biography: "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    rol: "Technical Writer",
+    biography: "Mi trabajo es tomar la información técnica y convertirla en contenido claro y accesible para todos. Colaboro estrechamente con mis compañeros para crear documentación precisa que nos ayude a mantenernos organizados y alineados.",
     socialMediaUrl: ["#","#","#"]
 }
 
 const MAURICIO_PROFILE = {
-    photo: "https://vignette1.wikia.nocookie.net/peaky-blinders/images/f/f5/Peaky_Tommy_Shelby.jpg/revision/latest?cb=20141013005757",
+    photo: "/src/home/components/team/img/jassiel.png",
     name: "Jassiel Mauricio",
-    rol: "Database Administrator",
-    biography: "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    rol: "Assistant",
+    biography: "Brindo apoyo en lo que se necesite para que el proyecto siga adelante. Me encargo de tareas diversas, asegurando que todo fluya sin interrupciones y que el equipo tenga lo necesario para continuar trabajando.",
     socialMediaUrl: ["#","#","#"]
 }
 
 const ALBERTO_PROFILE = {
-    photo: "https://unavatar.io/github/unknow",
+    photo: "/src/home/components/team/img/alberto.png",
     name: "José Alberto",
     rol: "Full-Stack Developer",
-    biography: "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    biography: "Soy responsable de crear, desarrollar y mantener nuestras herramientas digitales, tanto en el diseño como en la parte técnica. Me aseguro de que todos los componentes, tanto el front-end como el back-end, funcionen de manera integrada y eficiente, adaptándome según las necesidades.",
     socialMediaUrl: ["#","#","#"]
 }
 
